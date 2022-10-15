53eae21585f1f76b977de75ad9c4ae7fca7c8600
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2022-10-15 18:22:45 +0200

    Adding functionality for a single operation with positive integers

--- calculatorApp.js
+++ calculatorApp.js
@@ -4,12 +4,20 @@ function handleClick(button) {
     const operationField = document.getElementById("current-operation");
     operationField.innerHTML = operationField.innerHTML + button;
 }
-// Must implement a parser!!
+/* Must implement a good parser, to avoid using eval() !!
+* Currently only handles a single operator and the first number cannot be negative.
+* It also does not handle floats at input (which can result from operations).
+*/
 function handleResult() {
     const operationField = document.getElementById("current-operation");
     const currentOperation = operationField.innerHTML;
     if (validateOperation(currentOperation)){
-        alert("A Valid operation");
+        let result = 0;
+        const numbers = currentOperation.split(/[\+\-\*\/]/).map(e => parseInt(e));
+        const operators = currentOperation.split(/[\d]*/).filter(e => e !== "");
+        console.log(numbers);
+        console.log(operators);
+        operationField.innerHTML = singleOperation(numbers[0], numbers[1], operators[0]);
     }
     else{
         alert("Not a valid operation");
@@ -17,12 +25,35 @@ function handleResult() {
 }
 
 function validateOperation(str) {
-    let operators = 0;
+    if (str[0] === "*" || str[0] === "/")
+        return (false);
+    let operatorCount = 0;
     for (let i = 0; i < str.length; i++){
         if (!isDigit(str[i]))
-            operators++;
+            operatorCount++;
     }
-    return (operators > 1 ? false : true);
+    return (operatorCount > 1 ? false : true);
+}
+
+function singleOperation(a, b, op) {
+    let result;
+    switch (op) {
+        case "+":
+            result = a + b;
+            break;
+        case "-":
+            result = a - b;
+            break;
+        case "*":
+            result = a * b;
+            break;
+        case "/":
+            result = a / b;
+            break;
+        default:
+            break;
+    }
+    return (result);
 }
 
 function isDigit(c) {
